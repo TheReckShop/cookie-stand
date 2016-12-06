e901b25b5adf054bf619ef6ec30e274f81666e56
thereckshop <thereckshop@mac.com>
2016-12-05 16:57:34 -0800

    added sales.html and worked on app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+var firstAndPike = {
+  locationName: 'First and Pike',
+  minCustPerHour: 23,
+  maxCustPerHour: 65,
+  avgCookiesPerCust: 6.3,
+  randomCustPerHour: [],
+  totalCookiesSoldEachHour: [],
+  totalDailyCookieSales: 0,
+  calcRandomCustPerHour: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.randomCustPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+      console.log(this.randomCustPerHour[i]);
+    }
+  },
+}
