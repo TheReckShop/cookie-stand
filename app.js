9a65a408abc3ef511665c223f4e8110843cb42c2
thereckshop <thereckshop@mac.com>
2016-12-05 19:47:31 -0800

    added second function to location method

--- app.js
+++ app.js
@@ -8,52 +8,82 @@ var firstAndPike = {
   maxCustPerHour: 65,
   avgCookiesPerCust: 6.3,
   randomCustPerHour: [],
-  totalCookiesSoldEachHour: [],
+  totalCookiesSoldPerHour: [],
   totalDailyCookieSales: 0,
   calcRandomCustPerHour: function() {
     for (var i = 0; i < hours.length; i++) {
-      this.randomCustPerHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
-      console.log(this.randomCustPerHour[i]);
+      this.randomCustPerHour[i] = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
     }
   },
-}
+  calcTotalCookiesSoldEachHour: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.calcRandomCustPerHour();
+      this.totalCookiesSoldPerHour.push(Math.round(this.avgCookiesPerCust * this.randomCustPerHour[i]));
+      // console.log(this.totalCookiesSoldPerHour[i]);
+      this.totalDailyCookieSales += this.totalCookiesSoldPerHour[i];
+      console.log(this.totalDailyCookieSales);
+    }
+  },
+  render: function () {
+    firstAndPike.calcRandomCustPerHour();
+    firstAndPike.calcTotalCookiesSoldEachHour();
 
-var seaTacAirport = {
-  locationName: 'SeaTac Airport',
-  minCustPerHour: 3,
-  maxCustPerHour: 24,
-  avgCookiesPerCust: 1.2,
-  randomCustPerHour: [],
-  totalCookiesSoldEachHour: [],
-  totalDailyCookieSales: 0,
-}
 
-var seattleCenter = {
-  locationName: 'Seattle Center',
-  minCustPerHour: 11,
-  maxCustPerHour: 38,
-  avgCookiesPerCust: 3.7,
-  randomCustPerHour: [],
-  totalCookiesSoldEachHour: [],
-  totalDailyCookieSales: 0,
-}
+    var pikeHourlySales = document.getElementById('firstAndPike');
+    for (var i = 0; i < hours.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + this.totalCookiesSoldPerHour[i];
+      pikeHourlySales.appendChild(liEl);
+    }
+  }
+};
+firstAndPike.render();
 
-var capitolHill = {
-  locationName: 'Capitol Hill',
-  minCustPerHour: 20,
-  maxCustPerHour: 38,
-  avgCookiesPerCust: 2.3,
-  randomCustPerHour: [],
-  totalCookiesSoldEachHour: [],
-  totalDailyCookieSales: 0,
-}
 
-var alki = {
-  locationName: 'Alki',
-  minCustPerHour: 2,
-  maxCustPerHour: 16,
-  avgCookiesPerCust: 4.6,
-  randomCustPerHour: [],
-  totalCookiesSoldEachHour: [],
-  totalDailyCookieSales: 0,
-}
+
+
+// var seaTacAirport = {
+//   locationName: 'SeaTac Airport',
+//   minCustPerHour: 3,
+//   maxCustPerHour: 24,
+//   avgCookiesPerCust: 1.2,
+//   randomCustPerHour: [],
+//   totalCookiesSoldEachHour: [],
+//   totalDailyCookieSales: 0,
+// calcRandomCustPerHour: function() {
+//   for (var i = 0; i < hours.length; i++) {
+//     this.randomCustPerHour[i] = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
+//     console.log(this.randomCustPerHour[i]);
+//   }
+// },
+// }
+//
+// var seattleCenter = {
+//   locationName: 'Seattle Center',
+//   minCustPerHour: 11,
+//   maxCustPerHour: 38,
+//   avgCookiesPerCust: 3.7,
+//   randomCustPerHour: [],
+//   totalCookiesSoldEachHour: [],
+//   totalDailyCookieSales: 0,
+// }
+//
+// var capitolHill = {
+//   locationName: 'Capitol Hill',
+//   minCustPerHour: 20,
+//   maxCustPerHour: 38,
+//   avgCookiesPerCust: 2.3,
+//   randomCustPerHour: [],
+//   totalCookiesSoldEachHour: [],
+//   totalDailyCookieSales: 0,
+// }
+//
+// var alki = {
+//   locationName: 'Alki',
+//   minCustPerHour: 2,
+//   maxCustPerHour: 16,
+//   avgCookiesPerCust: 4.6,
+//   randomCustPerHour: [],
+//   totalCookiesSoldEachHour: [],
+//   totalDailyCookieSales: 0,
+// }
